924d2d6d4715f6af198049209cfd5d144f1910bd
dakhilmario <55787846+dakhilmario@users.noreply.github.com>
2021-02-15 09:50:46 +0100

    navbar in orde

--- js/hamburger_menu.js
+++ js/hamburger_menu.js
@@ -1,10 +1,13 @@
 "use strict"
 
 $(document).ready(function () {
-    $(".hammenu").click(function () { 
+    $(".c-hammenu").click(function () { 
         $(this).toggleClass('js-transformed');
         $(".nvbar__items").slideToggle(350, "linear");
-        $(".nvbar__items").css("display", "flex");
+        $("#js-nav-items").css("display", "flex");
     });
-
+    
+    window.addEventListener("resize", () => {
+        $("#js-nav-items").removeAttr('style');
+    });
 });
